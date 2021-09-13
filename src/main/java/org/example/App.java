a21125ab97cb0d0641ab18437efe0104b4bc5f35
DreDreDreDre <andrejar34@gmail.com>
2021-09-13 14:13:24 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,46 @@
+package org.example;
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        double firstNum;
+        double secondNum;
+        double thirdNum;
+        double highestNum = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        firstNum = sc.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        secondNum = sc.nextDouble();
+
+        System.out.print("Enter the third number: ");
+        thirdNum = sc.nextDouble();
+
+        if(firstNum != secondNum && firstNum != thirdNum && secondNum != thirdNum)
+        {
+            if(firstNum > secondNum && firstNum > thirdNum)
+            {
+                highestNum = firstNum;
+            }
+            else if(secondNum > firstNum && secondNum > thirdNum)
+            {
+                highestNum = secondNum;
+            }
+            else if(thirdNum > firstNum && thirdNum > secondNum)
+            {
+                highestNum = thirdNum;
+            }
+
+            System.out.println("The largest number is " + highestNum + ".");
+        }
+    }
+}
